64fee7ad54b18ddd0b40938285fcdc7e1ba1859a
Estela Martins <48722255+tamagoshii@users.noreply.github.com>
2020-12-11 19:06:25 -0300

    exercícios de fixação

---
+++ JAVA/olamundo/TestaWhile.java
@@ -0,0 +1,12 @@
+
+public class TestaWhile {
+	public static void main(String[] args) {
+
+		int contador = 5;
+
+		while (contador <= 15) {
+			System.out.println(contador);
+			contador ++; // contador +=1; contador = contador + 1; ou ++contador;
+		}
+	}
+}
