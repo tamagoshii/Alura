ee40f42699e411b83c8be93504115fa72dba96b8
Estela Martins <48722255+tamagoshii@users.noreply.github.com>
2020-12-11 19:07:31 -0300

    exercícios de fixação

---
+++ JAVA/olamundo/TestaSoma.java
@@ -0,0 +1,15 @@
+
+public class TestaSoma {
+	public static void main(String[] args) {
+
+		int contador = 0;
+		int soma = 0;
+		
+		while (contador <= 10) {
+			soma += contador;
+
+			System.out.println(soma);
+			contador++;
+		}
+	}
+}
