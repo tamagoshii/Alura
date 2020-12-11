4d665b676a4ca58978cd12c424f88456b30dbc5a
Estela Martins <48722255+tamagoshii@users.noreply.github.com>
2020-12-11 19:04:58 -0300

    Exercícios de fixação

---
+++ JAVA/olamundo/TestaVariavel.java
@@ -0,0 +1,12 @@
+
+public class TestaVariaveis {
+
+	public static void main(String[] args) {
+
+		System.out.println("Testando Variaveis");
+
+		int numero = 20;
+
+		System.out.println("Variavel numero eh: " + numero);
+	}
+}
