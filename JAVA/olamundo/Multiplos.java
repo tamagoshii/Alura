371db26f622627029bd77d2c53e517d319c31a85
Estela Martins <48722255+tamagoshii@users.noreply.github.com>
2020-12-11 19:11:50 -0300

    exercício de fixação

---
+++ JAVA/olamundo/Multiplos.java
@@ -0,0 +1,10 @@
+public class Multiplos {
+	public static void main(String[] args) {
+		System.out.println("Multiplos de 3:");
+		for(int i = 0; i <= 100; i++) {
+			if(i % 3 == 0) {
+				System.out.println(i);
+			}
+		}
+	}
+}
