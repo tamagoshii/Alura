eca520682f8009fd4422c07b17d1c39eef50ff45
Estela Martins <48722255+tamagoshii@users.noreply.github.com>
2020-12-11 19:06:58 -0300

    exercícios de fixação

---
+++ JAVA/olamundo/Triangulos.java
@@ -0,0 +1,29 @@
+
+public class Triangulos {
+	public static void main(String[] args) {
+		for(int i = 0; i <= 10; i++) {
+			for(int j = 0; j <= 10; j++) {
+				System.out.print("*");
+			}
+			System.out.println(" ");
+		}
+		
+		System.out.println();
+		
+		for(int i = 0; i <= 10; i++) {
+			for(int j = 0; j <= i; j++) {
+				System.out.print("*");
+			}
+			System.out.println(" ");
+		}
+		
+		System.out.println(" ");
+		
+		for(int i = 0; i <= 10; i++) {
+			for(int j = 10; j >= i; j--) {
+				System.out.print("*");
+			}
+			System.out.println(" ");
+		}
+	}
+}
