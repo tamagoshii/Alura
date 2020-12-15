389ec19c13c527bec3307e0587d9729330021604
Estela Martins <48722255+tamagoshii@users.noreply.github.com>
2020-12-15 13:28:06 -0300

    exercícios JAVA

---
+++ JAVA OO/bitybank/Herdado/TestaGerente.java
@@ -0,0 +1,19 @@
+public class TestaGerente {
+	public static void main(String[] args) {
+		Gerente novoGerente = new Gerente();
+		
+		novoGerente.setNome("Carol");
+		novoGerente.setCpf("123.456.789-99");
+		novoGerente.setSalario(7000.00);
+		
+		System.out.println("Nome: " + novoGerente.getNome());
+		System.out.println("CPF: " + novoGerente.getCpf());
+		System.out.println("Salario: " + novoGerente.getSalario());
+		
+		novoGerente.setSenha(2222);
+		boolean teste = novoGerente.autentica(2222);
+		
+		System.out.println(teste);
+		
+	}
+}
