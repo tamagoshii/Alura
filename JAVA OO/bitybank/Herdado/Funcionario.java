1406b4f3be286dfb11833f1a249b7735a5600df5
Estela Martins <48722255+tamagoshii@users.noreply.github.com>
2020-12-15 11:52:24 -0300

    exercícios JAVA

--- JAVA OO/bitybank/Herdado/Funcionario.java
+++ JAVA OO/bitybank/Herdado/Funcionario.java
@@ -4,10 +4,28 @@ public class Funcionario {
 		private String nome;
 		private String cpf;
 		private double salario;
+		private int tipo = 0; // 0 = comun, 1 = gerente e 2 = diretor;
 		
 		
 		public double bonificacao() {
-			return this.salario *0.1;
+			
+			if(this.tipo == 0 ) {
+				System.out.println("Comun");
+				return this.salario * 0.1;
+			} else if (this.tipo == 1) {
+				System.out.println("Gerente");
+				return this.salario * 0.3;
+			}
+				System.out.println("Diretor");
+				return this.salario * 0.5;
+		}
+		
+		public int getTipo() {
+			return tipo;
+		}
+		
+		public void setTipo(int tipo) {
+			this.tipo = tipo;
 		}
 		
 		public String getNome() {
