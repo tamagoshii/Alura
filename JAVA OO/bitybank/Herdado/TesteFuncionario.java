c3ab8440108819e6489a8d8174b9311e9a24dfc7
Estela Martins <48722255+tamagoshii@users.noreply.github.com>
2020-12-15 11:14:11 -0300

    exercícios JAVA

---
+++ JAVA OO/bitybank/Herdado/TesteFuncionario.java
@@ -0,0 +1,13 @@
+public class TestaFuncionario {
+	public static void main(String[] args) {
+		Funcionario novoFuncionario = new Funcionario();
+		
+		novoFuncionario.setNome("Funcionario");
+		novoFuncionario.setSalario(3000.00);
+		
+		System.out.println("Nome: " + novoFuncionario.getNome());
+		System.out.println("Salario: " + novoFuncionario.getSalario());
+		System.out.println("Bonificação: " + novoFuncionario.bonificacao());
+		
+	}
+}
