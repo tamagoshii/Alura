90ccb8315f9791d7a9ed15de0dd5feb4367b7ead
Estela Martins <48722255+tamagoshii@users.noreply.github.com>
2020-12-15 14:16:34 -0300

    Exercícios JAVA
    Acessando métodos e atributos da classe Pai

--- JAVA OO/bitybank/Herdado/Gerente.java
+++ JAVA OO/bitybank/Herdado/Gerente.java
@@ -11,12 +11,18 @@ public class Gerente extends Funcionario {
 		}
 	}
 	
-	// reescrita do metodo da classe pai
 	public double bonificacao() {
 		System.out.println("Cargo: Gerente");
-	return super.salario * 0.3; // "super" explicita que o atributo salario pertence a classe pai; 
+	return super.bonificacao() + super.getSalario(); // Acessamos o metodo bonificação e getSalario da classe pai atraves do "super";
 
 	}
+	
+//	// reescrita do metodo da classe pai
+//	public double bonificacao() {
+//		System.out.println("Cargo: Gerente");
+//	return super.salario * 0.3; // "super" explicita que o atributo salario pertence a classe pai; 
+//
+//	}
 
 	public int getSenha() {
 		return senha;
