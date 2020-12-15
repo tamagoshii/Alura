4488b15724e12c7bd580fe5a85ca29264ffe8f2f
Estela Martins <48722255+tamagoshii@users.noreply.github.com>
2020-12-15 13:55:25 -0300

    Exercícios JAVA
    Trabalhando com heranças

--- JAVA OO/bitybank/Herdado/Funcionario.java
+++ JAVA OO/bitybank/Herdado/Funcionario.java
@@ -1,50 +1,37 @@
 
 public class Funcionario {
 
-		private String nome;
-		private String cpf;
-		private double salario;
-		private int tipo = 0; // 0 = comun, 1 = gerente e 2 = diretor;
-		
-		
-		public double bonificacao() {
-			
-			if(this.tipo == 0 ) {
-				System.out.println("Comun");
-				return this.salario * 0.1;
-			} else if (this.tipo == 1) {
-				System.out.println("Gerente");
-				return this.salario * 0.3;
-			}
-				System.out.println("Diretor");
-				return this.salario * 0.5;
-		}
-		
-		public int getTipo() {
-			return tipo;
-		}
-		
-		public void setTipo(int tipo) {
-			this.tipo = tipo;
-		}
-		
-		public String getNome() {
-			return nome;
-		}
-		public void setNome(String nome) {
-			this.nome = nome;
-		}
-		public String getCpf() {
-			return cpf;
-		}
-		public void setCpf(String cpf) {
-			this.cpf = cpf;
-		}
-		public double getSalario() {
-			return salario;
-		}
-		public void setSalario(double salario) {
-			this.salario = salario;
-		}
-		
+	private String nome;
+	private String cpf;
+	protected double salario; // protected fica entre publico e privado | Somente visivel para herdeiros (classes filhas);
+
+	public double bonificacao() {
+		System.out.println("Cargo: Comun");
+		return this.salario * 0.1;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public double getSalario() {
+		return salario;
+	}
+
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+
 }
