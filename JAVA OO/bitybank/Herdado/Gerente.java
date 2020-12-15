0f2d01a1aad20dc797875bd46c94e76f43036c67
Estela Martins <48722255+tamagoshii@users.noreply.github.com>
2020-12-15 13:27:36 -0300

    exercícios JAVA

---
+++ JAVA OO/bitybank/Herdado/Gerente.java
@@ -0,0 +1,21 @@
+// Classe gerente herda os atributos da classe Funcionario.
+public class Gerente extends Funcionario {
+	
+	private int senha;
+	
+    public boolean autentica(int senha) {
+        if(this.senha == senha) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+	
+	public int getSenha() {
+		return senha;
+	}
+	
+	public void setSenha(int senha) {
+		this.senha = senha;
+	}
+}
