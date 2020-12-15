91d5332c3a3c615a96a5af32873660740b7d2042
Estela Martins <48722255+tamagoshii@users.noreply.github.com>
2020-12-15 13:56:03 -0300

    Exercícios JAVA
    Trabalhando com heranças

--- JAVA OO/bitybank/Herdado/Gerente.java
+++ JAVA OO/bitybank/Herdado/Gerente.java
@@ -1,20 +1,27 @@
 // Classe gerente herda os atributos da classe Funcionario.
 public class Gerente extends Funcionario {
-	
+
 	private int senha;
+
+	public boolean autentica(int senha) {
+		if (this.senha == senha) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 	
-    public boolean autentica(int senha) {
-        if(this.senha == senha) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-	
+	// reescrita do metodo da classe pai
+	public double bonificacao() {
+		System.out.println("Cargo: Gerente");
+	return super.salario * 0.3; // "super" explicita que o atributo salario pertence a classe pai; 
+
+	}
+
 	public int getSenha() {
 		return senha;
 	}
-	
+
 	public void setSenha(int senha) {
 		this.senha = senha;
 	}
